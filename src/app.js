3acddf7246184a8200f342c6ff2b119b8dd06669
lCherS <lucascherez.cl@gmail.com>
2020-09-22 15:00:01 -0300

    1 de 9: Criar Repositorio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+ const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -12,14 +12,27 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  // title, url e techs - body
+  // { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0}
+
+  const { title, url, techs } = request.body;
+  const data = {id: uuid, title, techs, likes: 0, url};
+  
+  console.log(data)
+  
+  repositories.push(data);
+
+  return response.json(data);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  // title, a url e as techs
 });
 
 app.delete("/repositories/:id", (request, response) => {
